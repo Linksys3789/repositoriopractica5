5e56f66f6aef6a311029b7adac0ded8f007d377c
Samantha <Samantha@192.168.1.163>
2016-06-14 02:14:09 +0100

    cuarta modificacion(añadiendo empleado + atributos)

---
+++ DAM_ED04_Actividad/src/dam_ed04_actividad/Empleado.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dam_ed04_actividad;
+
+/**
+ *
+ * @author Samantha
+ */
+public class Empleado {
+    
+    
+    protected String nombre;
+    protected String apellido;
+    protected String dni;
+
+   
+    
+}
