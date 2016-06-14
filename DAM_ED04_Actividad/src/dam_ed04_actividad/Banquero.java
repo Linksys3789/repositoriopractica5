eee229582063215c3a4e86db822a292dfc1dae6a
Samantha <Samantha@192.168.1.163>
2016-06-14 02:38:37 +0100

    sexta modificacion codigo (añadiendo banquero + atributos)

---
+++ DAM_ED04_Actividad/src/dam_ed04_actividad/Banquero.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dam_ed04_actividad;
+
+import java.util.Date;
+/**
+ *
+ * @author Samantha
+ */
+public class Banquero {
+     protected String nombre_Banco;
+    protected String direccion_Banco;
+    protected Date Horario_banco;
+    
+    
+}
