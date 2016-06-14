6aad7d62e4e55c06fb6fa7d2fb934413808a5592
Samantha <Samantha@192.168.1.163>
2016-06-14 02:43:20 +0100

    septima modificacion codigo (añadiendo banquero + getters y setters)

--- DAM_ED04_Actividad/src/dam_ed04_actividad/Banquero.java
+++ DAM_ED04_Actividad/src/dam_ed04_actividad/Banquero.java
@@ -14,6 +14,48 @@ public class Banquero {
      protected String nombre_Banco;
     protected String direccion_Banco;
     protected Date Horario_banco;
+
+    /**
+     * @return the nombre_Banco
+     */
+    public String getNombre_Banco() {
+        return nombre_Banco;
+    }
+
+    /**
+     * @param nombre_Banco the nombre_Banco to set
+     */
+    public void setNombre_Banco(String nombre_Banco) {
+        this.nombre_Banco = nombre_Banco;
+    }
+
+    /**
+     * @return the direccion_Banco
+     */
+    public String getDireccion_Banco() {
+        return direccion_Banco;
+    }
+
+    /**
+     * @param direccion_Banco the direccion_Banco to set
+     */
+    public void setDireccion_Banco(String direccion_Banco) {
+        this.direccion_Banco = direccion_Banco;
+    }
+
+    /**
+     * @return the Horario_banco
+     */
+    public Date getHorario_banco() {
+        return Horario_banco;
+    }
+
+    /**
+     * @param Horario_banco the Horario_banco to set
+     */
+    public void setHorario_banco(Date Horario_banco) {
+        this.Horario_banco = Horario_banco;
+    }
     
     
 }
